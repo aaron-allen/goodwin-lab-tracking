2d01468dcf6741ef376484a001b369c111581861
GoodwinTracking <thegoodwinlab@gmail.com>
2018-07-13 14:22:09 +0100

    Starting to add if statement to track whole plates.

--- 20180711_CompleteTrackingSrcipt.sh
+++ 20180711_CompleteTrackingSrcipt.sh
@@ -18,9 +18,9 @@
 #		5. Double check the coordinates for the spatial video cropping in lines 95-114 or 135-154.
 #		6. Double check the times for temporal video cropping in line 81.
 #
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #!/bin/bash
 
@@ -35,6 +35,8 @@ echo $today
 read -p "Enter the directory you with the files you wish to track:  " InputDirectory
 read -p "Enter the directory where you wish results to go:  " OutputDirectory
 read -p "Is the courtship plate upside down? (y/n)  " PlateOrientation
+read -p "Do you want the whole plate tracked as one video? (y/n) " WholePate 
+
 read -p "At what time point are the dividers retracted (in seconds): " StartTime
 read -p "How long do you want to track for (in seconds):  " Duration 
 
@@ -69,98 +71,108 @@ cd $today/
 ## ------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------------------
 sleep 5
-mkdir Cut/
-for A in "$InputDirectory/*.avi"
-do
-	echo Copying files from Synology
-	cp $A Temp/
-	for B in Temp/*.avi; do
-		ffmpeg -i $B -c copy -ss $StartTime -t $Duration Cut/~nB_10min.avi
-	done
-	rm Temp/*.avi
+#mkdir Cut/
+#for A in "$InputDirectory/*.avi"
+#do
+#	echo Copying files from Synology
+#	cp $A Temp/
+#	for B in Temp/*.avi; do
+#		ffmpeg -i $B -c copy -ss $StartTime -t $Duration Cut/~nB_10min.avi
+#	done
+#	rm Temp/*.avi
 done
 
 
 
 
 sleep 5
-if $PlateOrientation == "y" 
-then 
-	#UpsideDown
-	echo Cutting upside DOWN video.
-	for C in Cut/*.avi
-	do
-		mkdir IndividualVideos/~nC_IndividualVideos/
-		echo Cutting out individual arenas from: $C
-		ffmpeg -i $C -vf crop=480:480:195:50 IndividualVideos/~nC_IndividualVideos/~nC_vid01.avi
-		ffmpeg -i $C -vf crop=480:480:630:50 IndividualVideos/~nC_IndividualVideos/~nC_vid02.avi
-		ffmpeg -i $C -vf crop=480:480:1060:50 IndividualVideos/~nC_IndividualVideos/~nC_vid03.avi
-		ffmpeg -i $C -vf crop=480:480:1500:50 IndividualVideos/~nC_IndividualVideos/~nC_vid04.avi
-		ffmpeg -i $C -vf crop=480:480:1915:50 IndividualVideos/~nC_IndividualVideos/~nC_vid05.avi
-		ffmpeg -i $C -vf crop=480:480:1:350 IndividualVideos/~nC_IndividualVideos/~nC_vid06.avi
-		ffmpeg -i $C -vf crop=480:480:415:350 IndividualVideos/~nC_IndividualVideos/~nC_vid07.avi
-		ffmpeg -i $C -vf crop=480:480:850:350 IndividualVideos/~nC_IndividualVideos/~nC_vid08.avi
-		ffmpeg -i $C -vf crop=480:480:1285:350 IndividualVideos/~nC_IndividualVideos/~nC_vid09.avi
-		ffmpeg -i $C -vf crop=480:480:1710:350 IndividualVideos/~nC_IndividualVideos/~nC_vid10.avi
-		ffmpeg -i $C -vf crop=480:480:195:775 IndividualVideos/~nC_IndividualVideos/~nC_vid11.avi
-		ffmpeg -i $C -vf crop=480:480:630:775 IndividualVideos/~nC_IndividualVideos/~nC_vid12.avi
-		ffmpeg -i $C -vf crop=480:480:1060:775 IndividualVideos/~nC_IndividualVideos/~nC_vid13.avi
-		ffmpeg -i $C -vf crop=480:480:1500:775 IndividualVideos/~nC_IndividualVideos/~nC_vid14.avi
-		ffmpeg -i $C -vf crop=480:480:1915:775 IndividualVideos/~nC_IndividualVideos/~nC_vid15.avi
-		ffmpeg -i $C -vf crop=480:480:1:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid16.avi
-		ffmpeg -i $C -vf crop=480:480:415:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid17.avi
-		ffmpeg -i $C -vf crop=480:480:850:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid18.avi
-		ffmpeg -i $C -vf crop=480:480:1285:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid19.avi
-		ffmpeg -i $C -vf crop=480:480:1710:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid20.avi
-		echo Converting to ufmf
-		for D in IndividualVideos/~nC_IndividualVideos/*.avi
-		do
-			mkdir ufmf/~nC/Videos/~nD/
-			any2ufmf $D ufmf/~nC/Videos/~nD/~nD.ufmf ufmfCompressionParams.txt
-		done
-	done
-	rmdir /Q /S IndividualVideos/
-	rmdir /Q /S Temp/
-	rmdir /Q /S Cut/
-	rmdir /Q /S Crop/
-else
-	#UpsideUp
-	echo Cutting upside UP video.	
-	for C in [Cut/*.avi]; do
-		mkdir IndividualVideos/~nC_IndividualVideos/
-		echo Cutting out individual arenas from: $C
-		ffmpeg -i $C -vf crop=480:480:1:50 IndividualVideos/~nC_IndividualVideos/~nC_vid01.avi
-		ffmpeg -i $C -vf crop=480:480:415:50 IndividualVideos/~nC_IndividualVideos/~nC_vid02.avi
-		ffmpeg -i $C -vf crop=480:480:850:50 IndividualVideos/~nC_IndividualVideos/~nC_vid03.avi
-		ffmpeg -i $C -vf crop=480:480:1285:50 IndividualVideos/~nC_IndividualVideos/~nC_vid04.avi
-		ffmpeg -i $C -vf crop=480:480:1710:50 IndividualVideos/~nC_IndividualVideos/~nC_vid05.avi
-		ffmpeg -i $C -vf crop=480:480:195:350 IndividualVideos/~nC_IndividualVideos/~nC_vid06.avi
-		ffmpeg -i $C -vf crop=480:480:630:350 IndividualVideos/~nC_IndividualVideos/~nC_vid07.avi
-		ffmpeg -i $C -vf crop=480:480:1060:350 IndividualVideos/~nC_IndividualVideos/~nC_vid08.avi
-		ffmpeg -i $C -vf crop=480:480:1500:350 IndividualVideos/~nC_IndividualVideos/~nC_vid09.avi
-		ffmpeg -i $C -vf crop=480:480:1915:350 IndividualVideos/~nC_IndividualVideos/~nC_vid10.avi
-		ffmpeg -i $C -vf crop=480:480:1:775 IndividualVideos/~nC_IndividualVideos/~nC_vid11.avi
-		ffmpeg -i $C -vf crop=480:480:415:775 IndividualVideos/~nC_IndividualVideos/~nC_vid12.avi
-		ffmpeg -i $C -vf crop=480:480:850:775 IndividualVideos/~nC_IndividualVideos/~nC_vid13.avi
-		ffmpeg -i $C -vf crop=480:480:1285:775 IndividualVideos/~nC_IndividualVideos/~nC_vid14.avi
-		ffmpeg -i $C -vf crop=480:480:1710:775 IndividualVideos/~nC_IndividualVideos/~nC_vid15.avi
-		ffmpeg -i $C -vf crop=480:480:195:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid16.avi
-		ffmpeg -i $C -vf crop=480:480:630:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid17.avi
-		ffmpeg -i $C -vf crop=480:480:1060:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid18.avi
-		ffmpeg -i $C -vf crop=480:480:1500:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid19.avi
-		ffmpeg -i $C -vf crop=480:480:1915:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid20.avi
-		echo Converting to ufmf
-		for D in IndividualVideos/~nC_IndividualVideos/*.avi; do
-			mkdir ufmf/~nC/Videos/~nD/
-			any2ufmf D ufmf/~nC/Videos/~nD/~nD.ufmf ufmfCompressionParams.txt
-		done
-	done
-	rmdir /Q /S IndividualVideos/
-	rmdir /Q /S Temp/
-	rmdir /Q /S Cut/
-	rmdir /Q /S Crop/
-fi
 
+if $WholePate == "y"
+then
+
+
+
+
+else
+	if $PlateOrientation == "y" 
+	then 
+		#UpsideDown
+		echo Cutting upside DOWN video.
+		for C in Cut/*.avi
+		do
+			mkdir IndividualVideos/~nC_IndividualVideos/
+			echo Cutting out individual arenas from: $C
+			ffmpeg -i $C -vf crop=480:480:195:50 IndividualVideos/~nC_IndividualVideos/~nC_vid01.avi
+			ffmpeg -i $C -vf crop=480:480:630:50 IndividualVideos/~nC_IndividualVideos/~nC_vid02.avi
+			ffmpeg -i $C -vf crop=480:480:1060:50 IndividualVideos/~nC_IndividualVideos/~nC_vid03.avi
+			ffmpeg -i $C -vf crop=480:480:1500:50 IndividualVideos/~nC_IndividualVideos/~nC_vid04.avi
+			ffmpeg -i $C -vf crop=480:480:1915:50 IndividualVideos/~nC_IndividualVideos/~nC_vid05.avi
+			ffmpeg -i $C -vf crop=480:480:1:350 IndividualVideos/~nC_IndividualVideos/~nC_vid06.avi
+			ffmpeg -i $C -vf crop=480:480:415:350 IndividualVideos/~nC_IndividualVideos/~nC_vid07.avi
+			ffmpeg -i $C -vf crop=480:480:850:350 IndividualVideos/~nC_IndividualVideos/~nC_vid08.avi
+			ffmpeg -i $C -vf crop=480:480:1285:350 IndividualVideos/~nC_IndividualVideos/~nC_vid09.avi
+			ffmpeg -i $C -vf crop=480:480:1710:350 IndividualVideos/~nC_IndividualVideos/~nC_vid10.avi
+			ffmpeg -i $C -vf crop=480:480:195:775 IndividualVideos/~nC_IndividualVideos/~nC_vid11.avi
+			ffmpeg -i $C -vf crop=480:480:630:775 IndividualVideos/~nC_IndividualVideos/~nC_vid12.avi
+			ffmpeg -i $C -vf crop=480:480:1060:775 IndividualVideos/~nC_IndividualVideos/~nC_vid13.avi
+			ffmpeg -i $C -vf crop=480:480:1500:775 IndividualVideos/~nC_IndividualVideos/~nC_vid14.avi
+			ffmpeg -i $C -vf crop=480:480:1915:775 IndividualVideos/~nC_IndividualVideos/~nC_vid15.avi
+			ffmpeg -i $C -vf crop=480:480:1:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid16.avi
+			ffmpeg -i $C -vf crop=480:480:415:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid17.avi
+			ffmpeg -i $C -vf crop=480:480:850:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid18.avi
+			ffmpeg -i $C -vf crop=480:480:1285:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid19.avi
+			ffmpeg -i $C -vf crop=480:480:1710:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid20.avi
+			echo Converting to ufmf
+#  need to figure out how to change to ufmf on linux ... or just track .avi files
+#			for D in IndividualVideos/~nC_IndividualVideos/*.avi
+#			do
+#				mkdir ufmf/~nC/Videos/~nD/
+#				any2ufmf $D ufmf/~nC/Videos/~nD/~nD.ufmf ufmfCompressionParams.txt
+#			done
+		done
+		rmdir /Q /S IndividualVideos/
+		rmdir /Q /S Temp/
+		rmdir /Q /S Cut/
+		rmdir /Q /S Crop/
+	else
+		#UpsideUp
+		echo Cutting upside UP video.	
+		for C in [Cut/*.avi]; do
+			mkdir IndividualVideos/~nC_IndividualVideos/
+			echo Cutting out individual arenas from: $C
+			ffmpeg -i $C -vf crop=480:480:1:50 IndividualVideos/~nC_IndividualVideos/~nC_vid01.avi
+			ffmpeg -i $C -vf crop=480:480:415:50 IndividualVideos/~nC_IndividualVideos/~nC_vid02.avi
+			ffmpeg -i $C -vf crop=480:480:850:50 IndividualVideos/~nC_IndividualVideos/~nC_vid03.avi
+			ffmpeg -i $C -vf crop=480:480:1285:50 IndividualVideos/~nC_IndividualVideos/~nC_vid04.avi
+			ffmpeg -i $C -vf crop=480:480:1710:50 IndividualVideos/~nC_IndividualVideos/~nC_vid05.avi
+			ffmpeg -i $C -vf crop=480:480:195:350 IndividualVideos/~nC_IndividualVideos/~nC_vid06.avi
+			ffmpeg -i $C -vf crop=480:480:630:350 IndividualVideos/~nC_IndividualVideos/~nC_vid07.avi
+			ffmpeg -i $C -vf crop=480:480:1060:350 IndividualVideos/~nC_IndividualVideos/~nC_vid08.avi
+			ffmpeg -i $C -vf crop=480:480:1500:350 IndividualVideos/~nC_IndividualVideos/~nC_vid09.avi
+			ffmpeg -i $C -vf crop=480:480:1915:350 IndividualVideos/~nC_IndividualVideos/~nC_vid10.avi
+			ffmpeg -i $C -vf crop=480:480:1:775 IndividualVideos/~nC_IndividualVideos/~nC_vid11.avi
+			ffmpeg -i $C -vf crop=480:480:415:775 IndividualVideos/~nC_IndividualVideos/~nC_vid12.avi
+			ffmpeg -i $C -vf crop=480:480:850:775 IndividualVideos/~nC_IndividualVideos/~nC_vid13.avi
+			ffmpeg -i $C -vf crop=480:480:1285:775 IndividualVideos/~nC_IndividualVideos/~nC_vid14.avi
+			ffmpeg -i $C -vf crop=480:480:1710:775 IndividualVideos/~nC_IndividualVideos/~nC_vid15.avi
+			ffmpeg -i $C -vf crop=480:480:195:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid16.avi
+			ffmpeg -i $C -vf crop=480:480:630:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid17.avi
+			ffmpeg -i $C -vf crop=480:480:1060:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid18.avi
+			ffmpeg -i $C -vf crop=480:480:1500:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid19.avi
+			ffmpeg -i $C -vf crop=480:480:1915:1065 IndividualVideos/~nC_IndividualVideos/~nC_vid20.avi
+			echo Converting to ufmf
+#  need to figure out how to change to ufmf on linux ... or just track .avi files
+#			for D in IndividualVideos/~nC_IndividualVideos/*.avi; do
+#				mkdir ufmf/~nC/Videos/~nD/
+#				any2ufmf D ufmf/~nC/Videos/~nD/~nD.ufmf ufmfCompressionParams.txt
+#			done
+		done
+		rmdir /Q /S IndividualVideos/
+		rmdir /Q /S Temp/
+		rmdir /Q /S Cut/
+		rmdir /Q /S Crop/
+	fi
+fi
 
 
 
