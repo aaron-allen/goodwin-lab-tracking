d6e54cdd522ed595632c195d7538b8f752fe7ad0
Annika Rings <annika.rings@dpag.ox.ac.uk>
2018-07-16 16:15:40 +0100

    added functions for identity corrections in FlyTracker and JAABA outputs

---
+++ reassign_identities/assign_score_identities.m
@@ -0,0 +1,41 @@
+%Annika Rings July 2018
+%function for reassigning identities of flies in the JAABA output files
+%named 'scores*.mat'
+%USAGE: in matlab command window, type: assign_score_identities(scoresdir,idsfile)
+%scoresdir is the absolute or relative path to the input directory that
+%contains the scores files
+%idsfile is the file containing a structure with old and new ids of the
+%flies (as created by the assign_chambers function)
+
+%this function saves an id-corrected version of each scores file appending the
+%ending _id_corrected.mat in the scoresdir
+
+
+function assign_score_identities(scoresdir,idsfile)
+startdir=pwd;
+load(idsfile);
+cd (scoresdir);
+
+scoresfiles=dir('scores*.mat');
+scoresfilenames=arrayfun(@(f) f.name,scoresfiles,'UniformOutput',false);
+outputfilenames=cellfun(@(f) strrep(f,'.mat','_id_corrected.mat'),scoresfilenames,'UniformOutput',false);
+cellfun(@(scoresfilename,outputfilename) id_correct_scorefile(scoresfilename,outputfilename,ids),scoresfilenames,outputfilenames);
+
+cd(startdir);
+clear all;
+end
+function id_correct_scorefile(scoresfilename,outputfilename,ids)
+load(scoresfilename);
+scores=allScores.scores;
+postprocessed=allScores.postprocessed;
+id_new = ids.id_new;
+id_old = ids.id_old;
+for i=1:numel(id_old)
+postprocessed_new{1,id_new(i)}=postprocessed{1,id_old(i)};
+scores_new{1,id_new(i)}=scores{1,id_old(i)};
+end
+
+allScores.scores=deal(scores_new);
+allScores.postprocessed=deal(postprocessed_new);
+save(outputfilename,'allScores');
+end
---
+++ reassign_identities/reassign_identities.m
@@ -0,0 +1,29 @@
+%Annika Rings July 2018
+
+
+%This function calls the assign_chambers function and the
+%assign_score_identities function on inputdir
+%these 2 functions have to be added to the path or they have to be located
+%in the folder from which the function is executed
+%further dependencies: nestStructSort package is required in one of the
+%functions that i called from this function
+
+%USAGE: in matlab command window, type: reassign_identities(inputdir)
+%inputdir is the absolute or relative path to the input directory
+
+%inputdir must contain the following:
+%-a file named inputdir'-calibration.mat'
+%-a JAABA folder named inputdir'_JAABA'
+    %the JAABA folder must contain:
+    %-a file named trx.mat
+    %-a variable number of files named 'scores*.mat' (not necessary, but if
+    %they don't exist, only the trx file will be corrected)
+
+
+function reassign_identities(inputdir)
+    JAABAfolder=strcat(inputdir,'_JAABA');
+    JAABAfolder_path=fullfile(inputdir,JAABAfolder);
+    id_path=fullfile(inputdir,'ids.mat');
+    assign_chambers(inputdir);
+    assign_score_identities(JAABAfolder_path,id_path);
+end
