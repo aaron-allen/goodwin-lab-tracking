458ec2935a621e66ac064280413f3953ec56fe83
Annika Rings <annika.rings@dpag.ox.ac.uk>
2018-07-20 13:46:28 +0100

    changed error_handling_wrapper function to make it write the errorlogfile to the directory the function was called from instead of the current directory at the timepoint the error was raised

--- WholePlate/error_handling_wrapper.m
+++ WholePlate/error_handling_wrapper.m
@@ -1,12 +1,14 @@
 
 function error_handling_wrapper(errorlogfile,myfun,varargin)
     errorlogfile=string(errorlogfile);
+    currentdir=pwd;
     try
         f=str2func(myfun);
         f(varargin{:});
     catch ME
            errorMessage= ME.message;
            disp(errorMessage);
+           cd(currentdir);
            fidd = fopen(errorlogfile, 'a');
            fprintf(fidd,'could not execute function %s on %s: ', myfun,varargin{:}); % To file
            fprintf(fidd, '%s\n', errorMessage); % To file
