73570580669f5aa42f6b63d08b7fbcd9ead3b9e4
GoodwinTracking <thegoodwinlab@gmail.com>
2018-08-24 10:27:49 +0100

    added making the directory for the bg image to run_calibrator_non_interactive.m

--- WholePlate/run_calibrator_non_interactive.m
+++ WholePlate/run_calibrator_non_interactive.m
@@ -3,6 +3,7 @@ addpath(genpath('/home/goodwintracking/TheCompleteFlyTrackingBundle/WholePlate')
 videos=dir('*.ufmf');
 videonames={videos.name};
 videonamestr=cellfun(@(vid) strrep(vid,'.ufmf',''), videonames,'UniformOutput',false);
+cellfun(@(videoname) mkdir(videoname), videonamestr);
 calibfilenames=cellfun(@(vidname) strcat(vidname,'_calibration.mat'), videonamestr,'UniformOutput',false);
 cellfun(@(vid,calibfilename) error_handling_wrapper('calibration_errors.log','calibrator_non_interactive',char(vid),char(calibfilename)),videonames,calibfilenames,'UniformOutput',false);
 exit;
