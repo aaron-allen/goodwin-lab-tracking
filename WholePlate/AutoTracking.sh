792c5f5d82e8ef13667874dccd52a4d93a36606d
Annika Rings <annika.rings@dpag.ox.ac.uk>
2018-10-03 17:51:12 +0100

    added line in AutoTracking.sh to run script_fix_trk.m

--- WholePlate/AutoTracking.sh
+++ WholePlate/AutoTracking.sh
@@ -3,5 +3,6 @@
 /usr/local/bin/matlab -nodisplay -nosplash -r "AutoTracking"
 /usr/local/bin/matlab -nodisplay -nosplash -r "ApplyClassifiers"
 /usr/local/bin/matlab -nodisplay -nosplash -r "script_reassign_identities"
+/usr/local/bin/matlab -nodisplay -nosplash -r "script_fix_trk"
 
 exit
