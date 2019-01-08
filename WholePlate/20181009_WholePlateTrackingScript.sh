8d6bc79319ae209f825bb60600edded4861a1646
GoodwinTracking <thegoodwinlab@gmail.com>
2019-01-08 15:41:36 +0000

    Added if statement around the whole master script to test whether the input directory exists. There were issues when it didn't and the script got to the copy to synology section (it created a lot of duplication on the Synologys).

--- WholePlate/20181009_WholePlateTrackingScript.sh
+++ WholePlate/20181009_WholePlateTrackingScript.sh
@@ -28,169 +28,174 @@ echo $(date)
 
 # setting up a variable with todays date and making a folder for the modified courtship videos
 MasterDirectory=$(pwd)
-echo $MasterDirectory
+echo The Master Directory is: $MasterDirectory
 today=$(date +%Y%m%d)
-echo $today
+# echo $today
 today=${today}Tracked
-echo  "$today"
+# echo  "$today"
 
-#read -p "Enter the directory you with the files you wish to track:  " InputDirectory
-#read -p "Enter the directory where you wish results to go:  " WorkingDirectory
-InputDirectory=/mnt/Synology/ToBeTracked/*Converted
-WorkingDirectory=/mnt/LocalData/WorkingDirectory
-echo This is the input directory: $InputDirectory
-echo This is the ouptut directory: $WorkingDirectory
+if [ -d /mnt/Synology/ToBeTracked/*Converted ]; then
+	echo "Input directory to be tracked exists."
+	#read -p "Enter the directory you with the files you wish to track:  " InputDirectory
+	#read -p "Enter the directory where you wish results to go:  " WorkingDirectory
+	InputDirectory=/mnt/Synology/ToBeTracked/*Converted
+	WorkingDirectory=/mnt/LocalData/WorkingDirectory
+	echo This is the input directory: $InputDirectory
+	echo This is the ouptut directory: $WorkingDirectory
 
-pwd
-cd $WorkingDirectory/
-pwd
-mkdir $today/
-cd $today
-CurrentDirectory=$(pwd)
+	pwd
+	cd $WorkingDirectory/
+	pwd
+	mkdir $today/
+	cd $today
+	CurrentDirectory=$(pwd)
 
 
-# Track all video files with FlyTracker, and apply classifiers with JAABA
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo FILE TRANSFER AND TRACKING
-for Z in $InputDirectory/*.ufmf
-do
-	FileName=$(basename -a --suffix=.ufmf "$Z")
-	mkdir "$FileName"
-	echo Copying video files from Synology
-	cp "$Z" $WorkingDirectory/$today/"$FileName"/
-	cd "$FileName"
-	for A in *.ufmf
+	# Track all video files with FlyTracker, and apply classifiers with JAABA
+	# -------------------------------------------------------------------------------------------------------------------------------------------------------------
+	echo FILE TRANSFER AND TRACKING
+	for Z in $InputDirectory/*.ufmf
 	do
-		echo Copying Matlab files into tracking directory
-		cp -r $MasterDirectory/AutoTracking.sh $WorkingDirectory/$today/"$FileName"/AutoTracking.sh
-		cp -r $MasterDirectory/AutoTracking.m $WorkingDirectory/$today/"$FileName"/AutoTracking.m
-		cp -r $MasterDirectory/run_calibrator_non_interactive_xflies.m $WorkingDirectory/$today/"$FileName"/run_calibrator_non_interactive_xflies.m
-		cp -r $MasterDirectory/DeleteSingletonFlies.m $WorkingDirectory/$today/"$FileName"/DeleteSingletonFlies.m
-		cp -r $MasterDirectory/ApplyClassifiers.m $WorkingDirectory/$today/"$FileName"/ApplyClassifiers.m
-		cp -r $MasterDirectory/script_reassign_identities.m $WorkingDirectory/$today/"$FileName"/script_reassign_identities.m
-		/usr/local/bin/matlab  -r "run_calibrator_non_interactive_xflies"
-		cp -r  $WorkingDirectory/$today/"$FileName"/*_calibration.mat  $WorkingDirectory/$today/"$FileName"/calibration.mat
-		echo Now tracking: "$A"
-		bash AutoTracking.sh &
-		#gnome-terminal -- ./AutoTracking.sh &
+		FileName=$(basename -a --suffix=.ufmf "$Z")
+		mkdir "$FileName"
+		echo Copying video files from Synology
+		cp "$Z" $WorkingDirectory/$today/"$FileName"/
+		cd "$FileName"
+		for A in *.ufmf
+		do
+			echo Copying Matlab files into tracking directory
+			cp -r $MasterDirectory/AutoTracking.sh $WorkingDirectory/$today/"$FileName"/AutoTracking.sh
+			cp -r $MasterDirectory/AutoTracking.m $WorkingDirectory/$today/"$FileName"/AutoTracking.m
+			cp -r $MasterDirectory/run_calibrator_non_interactive_xflies.m $WorkingDirectory/$today/"$FileName"/run_calibrator_non_interactive_xflies.m
+			cp -r $MasterDirectory/DeleteSingletonFlies.m $WorkingDirectory/$today/"$FileName"/DeleteSingletonFlies.m
+			cp -r $MasterDirectory/ApplyClassifiers.m $WorkingDirectory/$today/"$FileName"/ApplyClassifiers.m
+			cp -r $MasterDirectory/script_reassign_identities.m $WorkingDirectory/$today/"$FileName"/script_reassign_identities.m
+			/usr/local/bin/matlab  -r "run_calibrator_non_interactive_xflies"
+			cp -r  $WorkingDirectory/$today/"$FileName"/*_calibration.mat  $WorkingDirectory/$today/"$FileName"/calibration.mat
+			echo Now tracking: "$A"
+			bash AutoTracking.sh &
+			#gnome-terminal -- ./AutoTracking.sh &
+		done
+		cd $CurrentDirectory
+		sleep 5s    # 5 second lag to allow MATLAB to open
+		
+		# Check if matlab is running
+		while [ $(pgrep -c "MATLAB") -gt 3 ]
+		do
+			sleep 2m
+		done
 	done
-	cd $CurrentDirectory
-	sleep 5s    # 5 second lag to allow MATLAB to open
-	
-	# Check if matlab is running
-	while [ $(pgrep -c "MATLAB") -gt 3 ]
+
+	# Wait for the tracking to finish before going on to next section
+	echo "Just waiting for the tracking to finish."
+	while pgrep -x "MATLAB" > /dev/null
 	do
 		sleep 2m
 	done
-done
-
-# Wait for the tracking to finish before going on to next section
-echo "Just waiting for the tracking to finish."
-while pgrep -x "MATLAB" > /dev/null
-do
-	sleep 2m
-done
 
 
-# Generate Diagnostic Plots
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo DIAGNOSTIC PLOT
-cp -r $MasterDirectory/DiagnosticPlots.m $WorkingDirectory/$today/DiagnosticPlots.m
-/usr/local/bin/matlab -nodisplay -nosplash -r "DiagnosticPlots"
-# Take all the individual pdfs of the Diagnotic plots and merge them into one pdf per video
-for P in */
-do
-	cd $P/Results
-	echo MERGING DIAGNOSTIC PLOTS INTO ONE PDF FOR $P
-	pdftk *.pdf cat output ${P%%/*}_DiagnosticPlots.pdf
-	rm *[0-9].pdf
-	cd $CurrentDirectory
-done
-
-# Extracting Data for Each Plate
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo EXTRACT DATA AND PDFs
-cp -r $MasterDirectory/ExtractData.m $WorkingDirectory/$today/ExtractData.m
-/usr/local/bin/matlab -nodisplay -nosplash -r "ExtractData"
-
-# Calculate indices with R
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo CALCULATE INDICES
-cp -r $MasterDirectory/CalculateIndices.R $WorkingDirectory/$today/CalculateIndices.R
-Rscript CalculateIndices.R
-
-
-
-# Clean up ...
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# Clean up of matlab files
-rm DiagnosticPlots.m
-rm ExtractData.m
-rm CalculateIndices.R
-for X in */
-do
-	cd "$X"/
-	rm DeleteSingletonFlies.m
-	rm ApplyClassifiers.m
-	rm AutoTracking.sh
-	rm AutoTracking.m
-	rm script_reassign_identities.m
-	rm run_calibrator_non_interactive_xflies.m
-	cd $CurrentDirectory
-done
-
-
-# Moving any error log files for the Diagnostic... and Extract...
-echo "Now moving any error log files"
-DiagnosticErrors=$(ls *DiagnosticPlot_errors.log 2> /dev/null | wc -l)
-if [ "$DiagnosticErrors" != "0" ]
-then
-	echo Diagnostic Plot errors exist
-	for L in *DiagnosticPlot_errors.log
+	# Generate Diagnostic Plots
+	# -------------------------------------------------------------------------------------------------------------------------------------------------------------
+	echo DIAGNOSTIC PLOT
+	cp -r $MasterDirectory/DiagnosticPlots.m $WorkingDirectory/$today/DiagnosticPlots.m
+	/usr/local/bin/matlab -nodisplay -nosplash -r "DiagnosticPlots"
+	# Take all the individual pdfs of the Diagnotic plots and merge them into one pdf per video
+	for P in */
 	do
-		Directory=${L%%DiagnosticPlot_errors.log}
-		mv $L $Directory/
+		cd $P/Results
+		echo MERGING DIAGNOSTIC PLOTS INTO ONE PDF FOR $P
+		pdftk *.pdf cat output ${P%%/*}_DiagnosticPlots.pdf
+		rm *[0-9].pdf
+		cd $CurrentDirectory
 	done
-else
-	echo No Diagnostic Plot errors
-fi
-ExtractError=$(ls *ExtractData_errors.log 2> /dev/null | wc -l)
-if [ "$ExtractError" != "0" ]
-then
-	echo Extract Data errors exist
-	for L in *ExtractData_errors.log
+
+	# Extracting Data for Each Plate
+	# -------------------------------------------------------------------------------------------------------------------------------------------------------------
+	echo EXTRACT DATA AND PDFs
+	cp -r $MasterDirectory/ExtractData.m $WorkingDirectory/$today/ExtractData.m
+	/usr/local/bin/matlab -nodisplay -nosplash -r "ExtractData"
+
+	# Calculate indices with R
+	# -------------------------------------------------------------------------------------------------------------------------------------------------------------
+	echo CALCULATE INDICES
+	cp -r $MasterDirectory/CalculateIndices.R $WorkingDirectory/$today/CalculateIndices.R
+	Rscript CalculateIndices.R
+
+
+
+	# Clean up ...
+	# -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	# Clean up of matlab files
+	rm DiagnosticPlots.m
+	rm ExtractData.m
+	rm CalculateIndices.R
+	for X in */
 	do
-		Directory=${L%%ExtractData_errors.log}
-		mv $L $Directory/
+		cd "$X"/
+		rm DeleteSingletonFlies.m
+		rm ApplyClassifiers.m
+		rm AutoTracking.sh
+		rm AutoTracking.m
+		rm script_reassign_identities.m
+		rm run_calibrator_non_interactive_xflies.m
+		cd $CurrentDirectory
 	done
+
+
+	# Moving any error log files for the Diagnostic... and Extract...
+	echo "Now moving any error log files"
+	DiagnosticErrors=$(ls *DiagnosticPlot_errors.log 2> /dev/null | wc -l)
+	if [ "$DiagnosticErrors" != "0" ]
+	then
+		echo Diagnostic Plot errors exist
+		for L in *DiagnosticPlot_errors.log
+		do
+			Directory=${L%%DiagnosticPlot_errors.log}
+			mv $L $Directory/
+		done
+	else
+		echo No Diagnostic Plot errors
+	fi
+	ExtractError=$(ls *ExtractData_errors.log 2> /dev/null | wc -l)
+	if [ "$ExtractError" != "0" ]
+	then
+		echo Extract Data errors exist
+		for L in *ExtractData_errors.log
+		do
+			Directory=${L%%ExtractData_errors.log}
+			mv $L $Directory/
+		done
+	else
+		echo No Extract Data Plot errors
+	fi
+
+	# Move the resulting tracking results to the Synology in each users folder
+	echo MOVING TRACKING RESULTS TO SYNOLOGY
+	for D in */
+	do
+		Directory=$D
+		User=${Directory%%-*}
+		RecordingDate=${Directory#*-}
+		VideoName=${RecordingDate#*-}
+		RecordingDate=${RecordingDate%%-*}
+		VideoName=${VideoName%%/}
+		echo This is the Directory: $Directory
+		echo This is the User: $User
+		echo This is the Recording Date: $RecordingDate
+		echo This is the Video Name: $VideoName
+		mkdir -p /mnt/Synology/Tracked/$User/$RecordingDate
+		cp -R $D /mnt/Synology/Tracked/$User/$RecordingDate/
+		cd $CurrentDirectory
+	 done
+
+	# Move the input direstory to the archive synology for backup
+	echo MOVING INPUT DIRECTORY TO ARCHIVE SYNOLOGY
+	mv $InputDirectory /mnt/Synology/Archive/uFMF
+
+	echo All Done.
+	echo $(date)
+	sleep infinity
 else
-	echo No Extract Data Plot errors
+	echo "No input directory to be tracked."
 fi
-
-# Move the resulting tracking results to the Synology in each users folder
-echo MOVING TRACKING RESULTS TO SYNOLOGY
-for D in */
-do
-	Directory=$D
-	User=${Directory%%-*}
-	RecordingDate=${Directory#*-}
-	VideoName=${RecordingDate#*-}
-	RecordingDate=${RecordingDate%%-*}
-	VideoName=${VideoName%%/}
-	echo This is the Directory: $Directory
-	echo This is the User: $User
-	echo This is the Recording Date: $RecordingDate
-	echo This is the Video Name: $VideoName
-	mkdir -p /mnt/Synology/Tracked/$User/$RecordingDate
-	cp -R $D /mnt/Synology/Tracked/$User/$RecordingDate/
-	cd $CurrentDirectory
- done
-
-# Move the input direstory to the archive synology for backup
-echo MOVING INPUT DIRECTORY TO ARCHIVE SYNOLOGY
-mv $InputDirectory /mnt/Synology/Archive/uFMF
-
-echo All Done.
-echo $(date)
-sleep infinity
