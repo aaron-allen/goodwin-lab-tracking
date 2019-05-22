fb7926fc6f9a45b13d939e5d4e74089d26b1296c
Annika Rings <annika.rings@dpag.ox.ac.uk>
2019-05-22 17:34:21 +0100

    added performance measurements and outputfile saving

--- optogenetic_detector/ison.m
+++ optogenetic_detector/ison.m
@@ -1,11 +1,23 @@
-function onframes=ison(videoname,numframes)
-[avimg,vinfo]=makeavimg(videoname);
-onframes=zeros(vinfo.n_frames,1);
+function [t0,t2,t4,onframes]=ison(videoname,numframes)
+outputfilename=strrep(videoname,'.ufmf','_onframes.mat');
+output_timings=strrep(videoname,'.ufmf','_performance.mat');
+tic
+[avimg,vinfo]=makeavimg(videoname,numframes);
+t0=toc;
+tic
 frames=min(numframes,vinfo.n_frames);
+onframes=zeros(frames,1);
+t2=toc;
+
 for id=0:(frames-1)
+    tic
     [im,idnew] = video_read_frame(vinfo, id);
-    numabovethresh=finddiff(avimg,im,0.35,2100,2400,700,900);
+    t4=toc;
+    numabovethresh=finddiff(avimg,im,0.35,2100,2400,600,1000);
     if numabovethresh>40
         onframes(id+1)=1;
     end
+    
 end
+save(outputfilename,'onframes');
+save(output_timings,'t0','t2','t4');
--- optogenetic_detector/makeavimg.m
+++ optogenetic_detector/makeavimg.m
@@ -1,8 +1,9 @@
-function [avimg,vinfo]=makeavimg(videoname)
+function [avimg,vinfo]=makeavimg(videoname,numframes)
 vinfo = video_open(videoname)
 avimg=zeros(vinfo.sx,vinfo.sy);
-for id=0:(vinfo.n_frames-1)
+frames=min(numframes,vinfo.n_frames);
+for id=0:(frames-1)
 [im,idnew] = video_read_frame(vinfo, id);
 avimg=avimg+im;
 end
-avimg=avimg./vinfo.n_frames;
+avimg=avimg./frames;
