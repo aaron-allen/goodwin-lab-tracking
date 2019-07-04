4137e1388e4b1ee69ba8f8f549da77dde9772e51
Aaron M. Allen <aaron.allen@dpag.ox.ac.uk>
2019-07-04 09:37:40 +0100

    xcopy error level code does not seem to be reliable. script was deleting file even when they did not transfer. added if statements to test for the existance of the remote video and check its size before deleting. also added code to automatically append settings files if transferring from two computers on the same day.

--- BehaviourRoom_CompressAndTransferToSynology_Temp.bat
+++ BehaviourRoom_CompressAndTransferToSynology_Temp.bat
@@ -1,12 +1,15 @@
 @echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
 date /T
 time /T
 
-:: Aaron M. Allen, updated 2019.03.06
+:: Aaron M. Allen, updated 2019.07.04
 :: Batch script to transfer videos to Synology.
 :: The raw video and settings.txt file to the '2P' Synology.
 
 set today=%date:~-4%_%date:~3,2%_%date:~0,2%
+set "WorDir=%cd%"
 mkdir %today%_Courtship
 set/P UserName=ENTER YOUR FIRST NAME: 
 set/P SettingsFile=ENTER THE FULL NAME TO YOUR SETTINGS FILE: 
@@ -33,18 +36,59 @@ if exist "D:\TransferLogFiles\lock.txt" (goto WAIT)
 type NUL > "D:\TransferLogFiles\lock.txt"
 
 for %%A in ("*.avi") do (
-	echo COPYING UNCOMPRESSED VIDEO "%%A" TO OTHER SYNOLOGY
+	echo.
+	echo.
+	echo.
+	echo Copying raw video "%%A" to 2P Synology
 	xcopy "%%A" "X:\TempRawVideos\%UserName%\%today%_Courtship\"
-	echo THE ERROR LEVEL EQUALS    %errorlevel%
-	if %errorlevel% EQU 0 (
-		echo NOW DELTING FILES
-		del "%%A"
+	echo THE ERROR LEVEL EQUALS    !errorlevel!
+
+	if !errorlevel! EQU 0 (
+		if exist "X:\TempRawVideos\%UserName%\%today%_Courtship\%%A" (
+			echo Remote file exists
+			set localsize="%%~zA"
+			set remotesize="X:\TempRawVideos\%UserName%\%today%_Courtship\%%~zA"
+				cd "X:\TempRawVideos\%UserName%\%today%_Courtship"
+				set remotesize="%%~zA"
+				cd %WorDir%
+			echo Local file size is !localsize!
+			echo Remote file size is !remotesize!
+
+			if !localsize! EQU !remotesize! (
+				echo Local and remote file sizes match
+				echo NOW DELETING FILES
+				del "%%A"
+			) else (
+				echo Local and remote file are different sizes
+				echo Not deleting local file
+			)
+		) else (
+			echo Remote file does NOT exist
+			echo Not deleting local file
+		)
+	) else (
+		echo error level is not zero
+		echo Not deleting local file
 	)
 )
-::xcopy "%SettingsFile%" "X:\TempRawVideos\%UserName%\%today%_Courtship\"
-echo f | xcopy /f /y "%SettingsFile%" "X:\TempRawVideos\%UserName%\%today%_Courtship\settings.txt"
+
+echo.
+echo.
+echo.
+
+if exist "P:\TempRawVideos\%UserName%\%today%_Courtship\settings.txt" (
+	echo. >> "P:\TempRawVideos\%UserName%\%today%_Courtship\settings.txt"
+	type %SettingsFile% >> "P:\TempRawVideos\%UserName%\%today%_Courtship\settings.txt"
+) else (
+	type "%SettingsFile%" > "P:\TempRawVideos\%UserName%\%today%_Courtship\settings.txt"
+)
+
 
 del "D:\TransferLogFiles\lock.txt"
+
+echo.
+echo.
+echo.
 
 echo All Done.
 date /T
