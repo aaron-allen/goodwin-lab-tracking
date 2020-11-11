b373c6d79c5235cad1a6a6496b1a50be3c0da789
Annika Rings <annika.rings@cncb.ox.ac.uk>
2020-11-11 18:04:49 +0000

    some more changes to DeleteSpecificFlies

--- DeleteSpecificFlies.m
+++ DeleteSpecificFlies.m
@@ -1,18 +1,20 @@
 function DeleteSpecificFlies(flynumbers)
 
+currentdir =pwd;
 
-        disp('Deleting data from perframe files:');
-        cd perframe
-        mkdir BackupPerframe
-        perframeDataFiles = dir('*.mat');
-        for P = 1:length(perframeDataFiles)
-            copyfile(perframeDataFiles(P).name, ['BackupPerframe/Backup_' perframeDataFiles(P).name]);
-            load(perframeDataFiles(P).name);
-            LM2=zeros(1,size(data,2));
-            LM2(flynumbmers)=1;
-            LM=boolean(LM2);
-            disp(['    ' perframeDataFiles(P).name]);
-            data(:,LM)=[];
-            save(perframeDataFiles(P).name, 'data', 'units');
-        end
-        
+
+disp('Deleting data from perframe files:');
+cd perframe
+mkdir BackupPerframe
+perframeDataFiles = dir('*.mat');
+for P = 1:length(perframeDataFiles)
+    copyfile(perframeDataFiles(P).name, ['BackupPerframe/Backup_' perframeDataFiles(P).name]);
+    load(perframeDataFiles(P).name);
+    LM2=zeros(1,size(data,2));
+    LM2(flynumbers)=1;
+    LM=boolean(LM2);
+    disp(['    ' perframeDataFiles(P).name]);
+    data(:,LM)=[];
+    save(perframeDataFiles(P).name, 'data', 'units');
+end
+cd(currentdir);
