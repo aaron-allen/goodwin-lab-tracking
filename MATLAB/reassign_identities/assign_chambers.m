732e1287ef0a18794bc1a55293eb484837329e94
Annika Rings <annika.rings@cncb.ox.ac.uk>
2022-02-02 11:28:25 +0000

    updated identity reassignment to work with variable number of chambers

--- MATLAB/reassign_identities/assign_chambers.m
+++ MATLAB/reassign_identities/assign_chambers.m
@@ -1,4 +1,3 @@
-
 %Annika Rings July 2018
 %function for reassigning chamber numbers and identities of flies that were
 %tracked by Flytracker
@@ -18,92 +17,109 @@
 %the reassign identities function automatically calls this function
 
 function assign_chambers(inputdir)
-    startdir=pwd;
-    cd (inputdir);
-    % calibfile=strcat(inputdir,'-calibration.mat');
-    calibfile='../calibration.mat';
-    JAABAfolder=strcat(inputdir,'_JAABA');
-    trxfile=fullfile(JAABAfolder,'trx.mat');
-    trxfile_new=strrep(trxfile,'.mat','_id_corrected.mat');
-    load(calibfile);
-    load(trxfile);
-    id_old=arrayfun(@(f) f.id,trx);
-    meanx=arrayfun(@(f) mean(rmmissing(f.x)),trx);
-    meany=arrayfun(@(f) mean(rmmissing(f.y)),trx);
-    meanx_c=num2cell(meanx);
-    [trx(:).meanx]=deal(meanx_c{:});
-    meany_c=num2cell(meany);
-    [trx(:).meany]=deal(meany_c{:});
-    centroid=sortrows(calib.centroids,1);
-    centroid(1:5,:)=sortrows(centroid(1:5,:),2);
-    centroid(6:10,:)=sortrows(centroid(6:10,:),2);
-    centroid(11:15,:)=sortrows(centroid(11:15,:),2);
-    centroid(16:20,:)=sortrows(centroid(16:20,:),2);
-    maxx_chambers=centroid(:,2)+round((calib.rois{1}(4))/2);
-    minx_chambers=centroid(:,2)-round((calib.rois{1}(4))/2);
-    maxy_chambers=centroid(:,1)+round((calib.rois{1}(4))/2);
-    miny_chambers=centroid(:,1)-round((calib.rois{1}(4))/2);
-    x=arrayfun(@(f) rmmissing(f.x),trx,'UniformOutput',false);
-    firstx=cellfun(@(f) f(1,1),x);
-    chambers=arrayfun(@(f) assign_one(f,maxy_chambers,miny_chambers,maxx_chambers,minx_chambers),trx);
-    startpos=left_right_assign(chambers,firstx);
-
-    id_new=assign_identity(chambers);
-    id_new_c=num2cell(id_new);
-    [trx(:).id]=deal(id_new_c{:});
-    fields = {'meanx','meany'};
-    trx = rmfield(trx,fields);
-    trx=nestedSortStruct(trx,'id');
-    [trx(:).startpos]=deal(startpos{:});
-
-    ids=struct('id_old',id_old,'id_new',id_new,'chambers',chambers);
-    save_assigned('ids',ids);
-    save(trxfile_new,'trx');
-
-    cd(startdir);
-    clear all;
+startdir = pwd;
+cd(inputdir);
+% calibfile=strcat(inputdir,'-calibration.mat');
+calibfile = '../calibration.mat';
+JAABAfolder = strcat(inputdir, '_JAABA');
+trxfile = fullfile(JAABAfolder, 'trx.mat');
+trxfile_new = strrep(trxfile, '.mat', '_id_corrected.mat');
+%load calibration file
+load(calibfile);
+%load trx file
+load(trxfile);
+%determine old ids assigned by the tracker
+id_old = arrayfun(@(f) f.id, trx);
+%determine mean x and y values of each fly
+meanx = arrayfun(@(f) mean(rmmissing(f.x)), trx);
+meany = arrayfun(@(f) mean(rmmissing(f.y)), trx);
+meanx_c = num2cell(meanx);
+%add meanx and meany to trx file
+[trx(:).meanx] = deal(meanx_c{:});
+meany_c = num2cell(meany);
+[trx(:).meany] = deal(meany_c{:});
+%determine number of rows and columns of chambers
+centroid = sortrows(calib.centroids, 1);
+numchambers = size(centroid,1);
+uniquecentroids = unique(centroid(:,1));
+numrows = size(uniquecentroids,1);
+numcolumns = ceil(numchambers/numrows);
+%sort the chambers by row first, then within the row sort by column
+for c = 1: size(uniquecentroids)
+    ychamber = uniquecentroids(c);
+    centroid(centroid(:,1) == ychamber,:) = sortrows(centroid(centroid(:,1) == ychamber,:),2);
 end
 
 
-function chambernumber=assign_one(f,maxy_chambers,miny_chambers,maxx_chambers,minx_chambers)
+maxx_chambers = centroid(:, 2) + round((calib.rois{1}(4))/2);
+minx_chambers = centroid(:, 2) - round((calib.rois{1}(4))/2);
+maxy_chambers = centroid(:, 1) + round((calib.rois{1}(4))/2);
+miny_chambers = centroid(:, 1) - round((calib.rois{1}(4))/2);
+x = arrayfun(@(f) rmmissing(f.x), trx, 'UniformOutput', false);
+firstx = cellfun(@(f) f(1, 1), x);
 
-    for i=1:20
-        if f.meany<maxy_chambers(i)&&f.meany>miny_chambers(i)&&f.meanx<maxx_chambers(i)&&f.meanx>minx_chambers(i)
-            chambernumber=i;
+chambers = arrayfun(@(f) assign_one(f, maxy_chambers, miny_chambers, maxx_chambers, minx_chambers, numchambers), trx);
+startpos = left_right_assign(chambers, firstx, numchambers);
+
+
+id_new = assign_identity(chambers, calib.n_flies);
+id_new_c = num2cell(id_new);
+[trx(:).id] = deal(id_new_c{:});
+fields = {'meanx', 'meany'};
+trx = rmfield(trx, fields);
+trx = nestedSortStruct(trx, 'id');
+[trx(:).startpos] = deal(startpos{:});
+
+ids = struct('id_old', id_old, 'id_new', id_new, 'chambers', chambers);
+save('ids', 'ids');
+save(trxfile_new, 'trx');
+
+cd(startdir);
+clear all;
+end
+
+
+function chambernumber = assign_one(f, maxy_chambers, miny_chambers, maxx_chambers, minx_chambers, numchamb)
+%assigns the chamber number according to the x value to the flies
+for i = 1:numchamb
+    if f.meany < maxy_chambers(i) && f.meany > miny_chambers(i) && f.meanx < maxx_chambers(i) && f.meanx > minx_chambers(i)
+        chambernumber = i;
+    end
+end
+end
+
+function id_new = assign_identity(chambers, nflies)
+id_new = zeros(1,numel(chambers));
+nchambers = ceil(numel(chambers)/nflies);
+nums = [1:nflies*nchambers];
+individuals = transpose(reshape(nums,[nflies,nchambers]));
+for i = 1:nchambers
+   
+        id_new(find(chambers == i)) = individuals(i,:);
+    
+end
+end
+
+
+
+function startpos = left_right_assign(chambers, firstx, numchambs)
+%assigns startposition 'l' for left or 'r' for right to flies 
+%according to their position in the first frame of the tracking
+startpos = {};
+for i = 1:numchambs
+    firstxi = firstx(chambers == i);
+    if numel(firstxi) == 2
+        if firstxi(1) < firstxi(2)
+
+            startpos{(size(startpos, 1) + 1), 1} = {'l'};
+            startpos{(size(startpos, 1) + 1), 1} = {'r'};
+        else
+            startpos{(size(startpos, 1) + 1), 1} = {'r'};
+            startpos{(size(startpos, 1) + 1), 1} = {'l'};
         end
+    elseif numel(firstxi) == 1
+        startpos{(size(startpos, 1) + 1), 1} = {'l'};
     end
+
 end
-
-function id_new=assign_identity(chambers)
-    individuals=[1 2; 3 4; 5 6; 7 8; 9 10; 11 12; 13 14; 15 16; 17 18; 19 20; ...
-                 21 22; 23 24; 25 26; 27 28; 29 30; 31 32; 33 34; 35 36; 37 38; 39 40];
-    flag(1:20)=1;
-    for i=1:numel(chambers)
-        id_new(i)=individuals(chambers(i),flag(chambers(i))); flag(chambers(i))=2;
-    end
-end
-
-function save_assigned(filename,ids)
-
-    save(filename,'ids');
-end
-
-function startpos=left_right_assign(chambers,firstx)
-    startpos={};
-    for i=1:20
-        firstxi=firstx(chambers==i);
-        if numel(firstxi)==2
-            if firstxi(1)<firstxi(2)
-
-                startpos{(size(startpos,1)+1),1}={'l'};
-                startpos{(size(startpos,1)+1),1}={'r'};
-            else
-                startpos{(size(startpos,1)+1),1}={'r'};
-                startpos{(size(startpos,1)+1),1}={'l'};
-            end
-        elseif numel(firstxi)==1
-            startpos{(size(startpos,1)+1),1}={'l'};
-        end
-
-    end
 end
