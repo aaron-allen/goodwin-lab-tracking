6d7c69c23335468c7300f8a95e9eb60e2b2b787c
Aaron M. Allen <aaron.allen@dpag.ox.ac.uk>
2022-10-31 11:29:53 +0000

    added/updated cron scripts

--- bash/cron_weekday.sh
+++ bash/cron_weekday.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "\n\n\n######################\n\n\n"
+printf "\n\n\n######################\nWeekday Run\n"
 
 today=$(date +%Y%m%d-%H%M%S)
 num_vid=$( wc -l < "/mnt/synology/ToBeTracked/VideosFromStations/list_of_videos.csv" )
@@ -8,18 +8,19 @@ used_mem=$( free --giga | awk '0 == NR % 2' | awk '{print $3}' )
 
 printf "today=$today\n"
 printf "num_vid=$num_vid\n"
-printf "used_mem=$used_mem\n"
+printf "used_mem=$used_mem\n\n"
 
-printf "\n\n\n######################\n\n\n"
-
-
-if [[ "${num_vid}" -le 10 && "${used_mem}" -le 70 ]]; then
-	printf "good to go!\n"
-	cd /home/aaron/Documents/GitHub/goodwin-lab-tracking/bash/
-	bash GoodwinLab_Tracking.sh
-	# bash GoodwinLab_Tracking.sh >> "/mnt/data/Tracking/_logs/cron_logs/${today}_tracking_pipeline_cron.log" 2>&1
+if [[ "${num_vid}" -ge 0 ]]; then
+	if [[ "${num_vid}" -le 10 && "${used_mem}" -le 70 ]]; then
+		printf "good to go!\n"
+		cd /home/aaron/Documents/GitHub/goodwin-lab-tracking/bash/
+		# bash GoodwinLab_Tracking.sh
+		bash GoodwinLab_Tracking.sh >> "/mnt/data/Tracking/_logs/cron_logs/${today}_tracking_pipeline_cron.log" 2>&1
+	else
+		printf "NOT good to go ...\n"
+	fi
 else
-	printf "NOT good to go ...\n"
+	printf "No videos to track ...\n"
 fi
 
-printf "\n\n\n######################\n\n\n"
+printf "######################\n\n\n"
--- bash/cron_weekend.sh
+++ bash/cron_weekend.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-printf "\n\n\n######################\n\n\n"
+printf "\n\n\n######################\nWeekend Run\n\n\n"
 
 today=$(date +%Y%m%d-%H%M%S)
 printf "today=$today\n"
 
 cd /home/aaron/Documents/GitHub/goodwin-lab-tracking/bash/
-bash GoodwinLab_Tracking.sh
-# bash GoodwinLab_Tracking.sh >> "/mnt/data/Tracking/_logs/cron_logs/${today}_tracking_pipeline_cron.log" 2>&1
+# bash GoodwinLab_Tracking.sh
+bash GoodwinLab_Tracking.sh >> "/mnt/data/Tracking/_logs/cron_logs/${today}_tracking_pipeline_cron.log" 2>&1
 
 printf "\n\n\n######################\n\n\n"
